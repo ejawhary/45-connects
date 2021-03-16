1ae16c6cd86c28b518542865e18aa574451d6090
ejawhary <eamonnjawhary@gmail.com>
2021-03-16 20:35:40 +0400

    inital commit

---
+++ src/components/assets/cards/CardFront.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react';
+
+const CardFront = () => {
+	return (
+		<Fragment>
+			<div className="card-container">
+				<div className="card black-text">
+					<div className="center">
+						<div className="ff red-text">45</div>
+						<div className="connects black-text">Connects</div>
+					</div>
+				</div>
+			</div>
+			<div className="card-container">
+				<div className="card black-text">
+					<div className="center">
+						<div className="ff red-text">45</div>
+						<div className="connects black-text">Connects</div>
+					</div>
+				</div>
+			</div>
+			<div className="card-container">
+				<div className="card black-text">
+					<div className="center">
+						<div className="ff red-text">45</div>
+						<div className="connects black-text">Connects</div>
+					</div>
+				</div>
+			</div>
+			<div className="card-container">
+				<div className="card black-text">
+					<div className="center">
+						<div className="ff red-text">45</div>
+						<div className="connects black-text">Connects</div>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+export default CardFront;
